508e64a9e75c73d58e5b0f8d4d84bcbb17e5786a
Suji Falcon <suji.falcon@gmail.com>
2022-09-26 13:46:45 -0400

    finally finished

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Game-of-War-Project-2
-card game: war. no user input purely just computer rng vibes
+
+## Description
+
+card game called Game of War. no user input purely just computer rng vibes. compares cards between two players and commences war if two players have the same card
+
+### Dependencies
+
+javascript/html.
+
+### Executing program
+
+inspect element on browser and check the console. the full game viewable in console log
--- wargame.js
+++ wargame.js
@@ -1,50 +1,124 @@
-class warGame {
-  constructor(playerOne, playerTwo, deck) {
-    this.playerOne = playerOne;
-    this.playerTwo = playerTwo;
-    this.deck = deck;
-  }
-}
-
-class Player {
-  constructor(name, deck) {
-    this.name = name;
-    this.deck = deck;
-  }
-}
-
+// creates the faces for the cards and adds value for score
 class Card {
-  constructor(cardValue, value) {
-    this.cardValue = cardValue;
-    this.value = value;
+  constructor(suit, rank, score) {
+    this.suit = suit;
+    this.rank = rank;
+    this.score = score;
   }
 }
 
+// consists of cards and has formula for shuffling
 class Deck {
   constructor() {
     this.cards = [];
-    this.cardDeck();
-    this.cardShuffle();
+    this.makeDeck();
+    this.shuffleCards();
   }
 
-  cardDeck() {
-    const cardValue = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]; //possible to have 11-14 be strings like "Jack", "Queen", "King", "Ace" ??
+  // sets the suits and ranks for cards and then loops through them in a for loop, making 52 cards
+  makeDeck = () => {
+    const suits = ['♥︎', '♠︎', '♣︎', '♦︎'];
+    const ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
 
-    for (let i = 2; i < cardValue.length; i++) {
-      for (let j = 0; j < 4; j++) {
-        this.cardDeck.push(new Card(cardValue[i], i + 2));
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        this.cards.push(new Card(suits[i], ranks[j], j + 2));
       }
     }
-    console.log(this.cardDeck);
-  }
+    return this.cards;
+  };
 
-  // Fischer Yates Shuffling Algorithm
-  cardShuffle = () => {
+  //The Fisher-Yates algorithm to shuffle the cards array
+  shuffleCards() {
     for (let i = this.cards.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let tempCards = this.cards[i];
-      this.cards[i] = this.cards[j];
-      this.cards[j] = tempCards;
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    }
+  }
+}
+
+// has formula for the actual playing of the game
+class WarGame {
+  constructor() {
+    this.player1 = [];
+    this.player2 = [];
+    this.pile = [];
+    this.setup();
+    this.rounds = 1;
+  }
+  setup = () => {
+    const deck = new Deck();
+    let cards = deck.cards;
+
+    this.player1.push(...cards.slice(0, cards.length / 2));
+    this.player2.push(...cards.slice(cards.length / 2));
+  };
+  game = () => {
+    console.log(`Round: ${this.rounds}`);
+    while (this.player1.length > 0 && this.player2.length > 0) {
+      let p1Cc = this.player1.pop();
+      let p2Cc = this.player2.pop();
+
+      if (p1Cc.score > p2Cc.score) {
+        this.rounds += 1;
+        this.player1.unshift(p2Cc, p1Cc, ...this.pile);
+        console.log(
+          `Player One Cards: ${p1Cc.rank} of ${p1Cc.suit} || Player Two Cards: ${p2Cc.rank} of ${p2Cc.rank}. === Player One Wins! ===`,
+        );
+        console.log(
+          `Player One Cards: ${this.player1.length} || Player Two Cards: ${this.player2.length}`,
+        );
+        console.log(``);
+        console.log(``);
+        this.pile.length = 0;
+      } else if (p2Cc.score > p1Cc.score) {
+        this.rounds += 1;
+        this.player2.unshift(p1Cc, p2Cc, ...this.pile);
+        console.log(
+          `Player Two Cards: ${p2Cc.rank} of ${p2Cc.suit} || Player One Cards: ${p1Cc.rank} of ${p1Cc.rank}. === Player Two Wins! ===`,
+        );
+        console.log(
+          `Player Two Cards: ${this.player2.length} ||  Player One Cards: ${this.player1.length}`,
+        );
+        console.log(``);
+        console.log(``);
+        this.pile.length = 0;
+      } else {
+        this.rounds += 1;
+        console.log(
+          `Player One has ${p1Cc.rank} and Player Two has a ${p2Cc.rank}. Let the battle begin!`,
+        );
+        this.war(p1Cc, p2Cc);
+      }
+    }
+
+    if (this.player1.length <= 0) {
+      console.log(`Player Two Wins!`);
+    } else {
+      console.log(`Player One Wins!`);
+    }
+  };
+
+  war = (warCard1, warCard2) => {
+    console.log(
+      `We have ${this.pile.length} cards in the arena. Let the battle commence!`,
+    );
+    this.pile.push(warCard1, warCard2);
+    if (this.player1.length >= 4 && this.player2.length >= 4) {
+      this.pile.push(...this.player1.splice(this.player1.length - 3, 3));
+      this.pile.push(...this.player2.splice(this.player2.length - 3, 3));
+      console.log(this.pile);
+    } else if (this.player1.length > 4 && this.player2.length < 4) {
+      this.player1.unshift(...this.pile);
+      this.player1.unshift(...this.player2.splice(0));
+      this.pile.length = 0;
+    } else if (this.player2.length > 4 && this.player1.length < 4) {
+      this.player2.unshift(...this.pile);
+      this.player2.unshift(...this.player1.splice(0));
+      this.pile.length = 0;
     }
   };
 }
+
+let newRound = new WarGame();
+newRound.game();
